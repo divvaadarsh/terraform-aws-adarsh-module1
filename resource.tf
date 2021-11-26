ceceaeab5136c44a9b6019b02242fec23589ca2f
divvaadarsh <95043495+divvaadarsh@users.noreply.github.com>
2021-11-26 17:16:18 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -19,18 +19,8 @@ variable "nw-name" {
 resource "aws_instance" "server1" {
   ami           = var.image
   instance_type = var.hw
-  key_name = "test"
   tags = {
     Name = var.name
     Env  = "Prod"
-  }
-}
-
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = var.nw-name
   }
 }
